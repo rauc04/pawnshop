ac98300946b8218bb559b90fd7f62eaee18dd80e
RChinCaamal <raulchincaamal@outlook.com>
2022-04-03 20:39:22 -0500

    [feat] calculate new contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,95 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [customerPrice, setCustomerPrice] = useState(0)
+  const [pawnPrice, setPawnPrice] = useState(0)
+  const [contractValue, setContractValue] = useState(0)
+  const [percentCustomer, setPercentCustomer] = useState(0)
+  const [chargeValue, setChargeValue] = useState(0)
+  const [isOlder, setIsOlder] = useState(false)
+
+  const calculateValue = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault()
+
+    /**
+     * Calculate 20% customer
+     */
+    const percentCustomerLocal = (20 * customerPrice ) / 100
+
+    /**
+     * Calculate difference between `Customer price` and `Pawn price`
+     */
+    const difference = customerPrice - pawnPrice;
+    
+    if (difference > percentCustomerLocal) {
+      setChargeValue(() => difference.toFixed(2) as unknown as number)
+      setIsOlder(true)
+      setContractValue(() => pawnPrice.toFixed(2) as unknown as number)
+      setPercentCustomer(() => percentCustomerLocal.toFixed(2) as unknown as number)
+    } else {
+      setPercentCustomer(() => difference.toFixed(2) as unknown as number)
+      setChargeValue(() => percentCustomerLocal.toFixed(2) as unknown as number)
+      setContractValue(() => (customerPrice - percentCustomerLocal).toFixed(2) as unknown as number)
+      setIsOlder(false)
+    }
+  }
+
+  const clearInput = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault()
+
+    setPercentCustomer(() => 0)
+    setChargeValue(() => 0)
+    setContractValue(() => 0)
+    setIsOlder(false)
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Casa de empeño</h1>
+      <div className='wrapper'>
+        <div className='content-inputs'>
+          <div className='input-group'>
+            <label className='label'>Precio cliente</label>
+            <input type='number' className='input' onChange={ event => setCustomerPrice(Number(event.target.value)) } />
+          </div>
+          <div className='input-group'>
+            <label className='label'>Precio empeño</label>
+            <input type='number' className='input' onChange={ event => setPawnPrice(Number(event.target.value)) } />
+          </div>
+          <div className='buttons-wrapper'>
+            {
+              contractValue > 0 && 
+              <button className='btn btn-default' onClick={ (event) => clearInput(event) }>
+                Reiniciar
+              </button>
+            }
+            <button className='btn btn-primary' onClick={ (event) => calculateValue(event) }>
+              Calcular
+            </button>
+          </div>
+          
+        </div>
+        <div className='content-result'>
+          <div>
+            <span className='new-contract-label'>El nuevo contrato es de:</span>
+            <div className='content-price'>
+              <span className='sign'>$</span>
+              <span className='price'>{ contractValue }</span>
+            </div>
+          </div>
+          <div className='content-detail'>
+            <div className='detail-item'>
+              <span className='detail-label'>Vas a cobrar</span>
+              <span className='detail-value'>${ chargeValue }</span>
+            </div>
+            <div className='detail-item'>
+              <span className='detail-label'>{ isOlder ? 'El 20% es' : 'Menor al 20%' }</span>
+              <span className='detail-value'>${ percentCustomer }</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
